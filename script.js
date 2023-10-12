1f4f3d3f86d4838b899756e983ad18662874b909
QuitasolKenlei <146052692+QuitasolKenlei@users.noreply.github.com>
2023-10-12 12:13:34 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let password = " p@ssw0rd@2023";
+
+if (password.length >= 8 && password.includes("p@ssword")) {
+    console.log("Valid Password");
+} else {
+    console.log("Invalid Password");
+}
